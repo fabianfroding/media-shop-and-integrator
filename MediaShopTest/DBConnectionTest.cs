e0069d7fdc082396aee554d6629093851a2a32d7
fabianfroding <fabian.froding@gmail.com>
2020-08-25 10:54:52 +0200

    merge w/ unit test proj

---
+++ MediaShopTest/DBConnectionTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+
+namespace MediaShopTest
+{
+    [TestClass]
+    public class DBConnectionTest
+    {
+        [TestMethod]
+        public void TestDBConnection()
+        {
+            SqlConnection dbConnection = new SqlConnection(
+                                        "user id=FABIANLAPTOP\\Fabian2;" +
+                                       "password=password;" +
+                                        "server=localhost\\SQLEXPRESS2;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=MediaShop; " +
+                                       "connection timeout=30"
+            );
+
+            bool b = false;
+
+            try
+            {
+                dbConnection.Open();
+                b = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Assert.IsTrue(b);
+
+        }
+    }
+}
---
+++ MediaShopTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MediaShopTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MediaShopTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a5583f9-3536-42c7-9edd-c039269a213d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
